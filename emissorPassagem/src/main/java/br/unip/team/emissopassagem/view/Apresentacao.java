9edbedd8a406aac242bc8a7520d149363c0f1b44
Fernando <Fernando@DESKTOP-Fernando>
2019-11-08 23:02:38 -0300

    Tela inicial

--- emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/Apresentacao.java
+++ emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/Apresentacao.java
@@ -1,12 +1,31 @@
 package br.unip.team.emissopassagem.view;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 import br.unip.team.emissopassagem.model.persistencia.ConexaoDb;
 
-public class Apresentacao {
+public class Apresentacao extends JFrame {
+	
+	private JPanel basePane;
+	
+	public Apresentacao() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 500);
+		basePane = new JPanel();
+		basePane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		basePane.setLayout(null);
+		setContentPane(basePane);
+		setResizable(false);
+		setTitle("Ordenador de Coordenadas");
+		
+	}
 
 	public static void main(String[] args) {
 		new ConexaoDb();
-
+			
 	}
-
+		
 }
