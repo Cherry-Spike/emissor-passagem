e03d63ddee36bc8d3a5fe5fe579edf8b3565cbc1
Fernando <Fernando@DESKTOP-Fernando>
2019-11-11 23:40:51 -0300

    Validações Telas

--- emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/Telas/TelaEstacaoEb.java
+++ emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/Telas/TelaEstacaoEb.java
@@ -3,7 +3,13 @@ package br.unip.team.emissopassagem.view.Telas;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+
+import br.unip.team.emissopassagem.model.entidade.Estacao;
+import br.unip.team.emissopassagem.model.persistencia.EstacaoDAO;
 
 public class TelaEstacaoEb extends Tela {
 
@@ -20,11 +26,14 @@ public class TelaEstacaoEb extends Tela {
 		contentPane.setBackground(corDeFundo);
 		
 		setLabel(contentPane, "Escolha a estação de embarque", 170, 100, 500, 30, 20);		
-		setComboBox(contentPane, 170, 150, 300, 60);		
+		JComboBox <Object> cb = setComboBox(contentPane, 170, 150, 300, 60);
+		setComboItems(cb);		
+		JButton prox = setButtonProx(contentPane);
+		JButton cancel = setButtonCancel(contentPane);
 		
 		//Event Listener
 		
-		setButtonProx(contentPane).addActionListener(new ActionListener() {
+		prox.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				contentPane.hide();
@@ -32,7 +41,7 @@ public class TelaEstacaoEb extends Tela {
 			}
 		});
 		
-		setButtonCancel(contentPane).addActionListener(new ActionListener() {
+		cancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				contentPane.hide();
@@ -40,8 +49,24 @@ public class TelaEstacaoEb extends Tela {
 			}
 		});
 		
+		cb.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(cb.getSelectedItem() != null) {
+					prox.setEnabled(true);
+				}
+			}
+		});
+					
 		return contentPane;
 		
+	}
+	
+	public void setComboItems (JComboBox<Object> cb) {		
+		EstacaoDAO estacaoDAO = new EstacaoDAO();
+		for(Estacao estacao: estacaoDAO.obterTodos()) {
+			cb.addItem(estacao);
+		}		
 	}
 
 }
