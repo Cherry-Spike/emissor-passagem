96e7491e7d7fcdac1f20f31f599a1ccf4f13dc8d
Fernando <Fernando@DESKTOP-Fernando>
2019-11-20 01:13:13 -0300

    Fix

--- emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/tela/TelaPagamento.java
+++ emissorPassagem/src/main/java/br/unip/team/emissopassagem/view/tela/TelaPagamento.java
@@ -11,16 +11,19 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import br.unip.team.emissopassagem.controller.LogController;
+import br.unip.team.emissopassagem.controller.PassagemController;
 import br.unip.team.emissopassagem.model.entidade.Cartao;
 import br.unip.team.emissopassagem.model.entidade.VirtualKeyboard;
 
 public class TelaPagamento extends Tela<Object> {
 	private LogController logController = new LogController();
+	private PassagemController passagemController = new PassagemController();
 	private Cartao cartao = new Cartao();
 	private JButton prox;
 	private JTextField tfCartao, tfPin;
 	private int idItinerario;
-	private final int cartaoLimite = 16, pinLimite = 3;
+	private static final int CARTAO_LIMITE = 16;
+	private static final int PIN_LIMITE = 3;
 
 	public TelaPagamento(JPanel basePane, JPanel backPane, int idItinerario) {
 		this.idItinerario = idItinerario;
@@ -34,23 +37,27 @@ public class TelaPagamento extends Tela<Object> {
 
 		JPanel contentPane = new JPanel();
 		contentPane.setBackground(corDeFundo);
-		
+
 		prox = setButtonProx(contentPane);
 		JButton cancel = setButtonCancel(contentPane);
 		setLabel(contentPane, "Digite o numero do cartão:", 150, 40, 500, 30, 18);
-		tfCartao = setTextField(contentPane, 150, 90, 300, 50, cartaoLimite);
+		tfCartao = setTextField(contentPane, 150, 90, 300, 50, CARTAO_LIMITE);
 		setLabel(contentPane, "Digite o PIN:", 150, 185, 500, 30, 18);
-		tfPin = setTextField(contentPane, 150, 230, 100, 50, pinLimite);
-		
+		tfPin = setTextField(contentPane, 150, 230, 100, 50, PIN_LIMITE);
+
 		// Event Listener
 
 		prox.addActionListener(e -> {
-			setCartao();
+			cartao.setNumero(tfCartao.getText());
+			int pin = Integer.parseInt(tfPin.getText());
+			cartao.setPin(pin);
+
 			logController.adicionarLog(idItinerario, cartao);
+			int idPassgem = passagemController.adicionarPassagem(idItinerario);
 			contentPane.setVisible(false);
-			new TelaEmissao(basePane, backPane);
+			new TelaEmissao(basePane, backPane, idPassgem);
 		});
-				
+
 		cancel.addActionListener(e -> {
 			contentPane.setVisible(false);
 			backPane.setVisible(true);
@@ -63,18 +70,18 @@ public class TelaPagamento extends Tela<Object> {
 				VirtualKeyboard vk = new VirtualKeyboard();
 				vk.show(frame, contentPane);
 			}
-		});*/			
-		
-		tfCartao.addKeyListener(new KeyListener() {			
-						
+		});*/
+
+		tfCartao.addKeyListener(new KeyListener() {
+
 			public void keyTyped(KeyEvent e) {validaBtn();}
 			public void keyPressed(KeyEvent e) {}
 			public void keyReleased(KeyEvent e) {}
 
 		});
-		
-		tfPin.addKeyListener(new KeyListener() {			
-			
+
+		tfPin.addKeyListener(new KeyListener() {
+
 			public void keyTyped(KeyEvent e) {validaBtn();}
 			public void keyPressed(KeyEvent e) {}
 			public void keyReleased(KeyEvent e) {}
@@ -84,20 +91,11 @@ public class TelaPagamento extends Tela<Object> {
 		return contentPane;
 
 	}
-	
-	public void validaBtn() {
-		if(tfCartao.getText().length() >= cartaoLimite && tfPin.getText().length() >= pinLimite) {
+
+	public void validaBtn() {		
+		if (tfCartao.getText().length() >= CARTAO_LIMITE && tfPin.getText().length() >= PIN_LIMITE)
 			prox.setEnabled(true);
-		}else {
+		else
 			prox.setEnabled(false);
-		}
-	}
-	
-	public void setCartao() {	
-		
-		cartao.setNumero(tfCartao.getText());
-		int pin = Integer.parseInt(tfPin.getText());
-		cartao.setPin(pin);
-		
 	}
 }
